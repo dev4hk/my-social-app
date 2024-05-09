601b3ad47a4bb96a36dea0dffecb4680c02d02b3
Hoon <hoon0661@gmail.com>
2024-05-09 14:07:42 -0400

    initial commit

---
+++ src/main/java/org/example/mysocialapp/MySocialAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.mysocialapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySocialAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySocialAppApplication.class, args);
+    }
+
+}
