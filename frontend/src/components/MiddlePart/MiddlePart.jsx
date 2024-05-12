ea8b151cc37b787be7fe2e802ac13778995ac8de
Hoon <hoon0661@gmail.com>
2024-05-11 21:15:58 -0400

    added post create input

--- frontend/src/components/MiddlePart/MiddlePart.jsx
+++ frontend/src/components/MiddlePart/MiddlePart.jsx
@@ -1,11 +1,18 @@
-import { Avatar } from "@mui/material";
+import { Avatar, Card, IconButton } from "@mui/material";
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import StoryCircle from "./StoryCircle";
+import ImageIcon from "@mui/icons-material/Image";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import ArticleIcon from "@mui/icons-material/Article";
 
 const story = [1, 1, 1, 1, 1];
 
 const MiddlePart = () => {
+  const handleOpenCreatePostModel = () => {
+    console.log("open post model....");
+  };
+
   return (
     <div className="px-20">
       <section className="flex items-center p-5 rounded-b-md">
@@ -18,6 +25,41 @@ const MiddlePart = () => {
         {story.map((item, index) => (
           <StoryCircle key={index} />
         ))}
+      </section>
+
+      <section>
+        <Card className="p-5 mt-5">
+          <div className="flex justify-between">
+            <Avatar />
+            <input
+              className="outline-none w-[90%] rounded-full px-5 bg-transparent border-[#3b4054] border"
+              type="text"
+              readOnly
+            />
+          </div>
+          <div className="flex justify-center space-x-9 mt-5">
+            <div className="flex items-center">
+              <IconButton color="primary" onClick={handleOpenCreatePostModel}>
+                <ImageIcon />
+              </IconButton>
+              <span>Media</span>
+            </div>
+
+            <div className="flex items-center">
+              <IconButton color="primary" onClick={handleOpenCreatePostModel}>
+                <VideocamIcon />
+              </IconButton>
+              <span>Video</span>
+            </div>
+
+            <div className="flex items-center">
+              <IconButton color="primary" onClick={handleOpenCreatePostModel}>
+                <ArticleIcon />
+              </IconButton>
+              <span>Write Article</span>
+            </div>
+          </div>
+        </Card>
       </section>
     </div>
   );
