7a4e137760506d674b1fc5b4082dccc1ea7d7762
Hoon <hoon0661@gmail.com>
2024-05-16 11:48:36 -0400

    refactored method - load file

--- src/main/java/org/example/mysocialapp/response/PostResponse.java
+++ src/main/java/org/example/mysocialapp/response/PostResponse.java
@@ -2,21 +2,15 @@ package org.example.mysocialapp.response;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.example.mysocialapp.entity.Comment;
 import org.example.mysocialapp.entity.Post;
 import org.example.mysocialapp.entity.User;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
+
+import static org.example.mysocialapp.util.FileUtils.loadFileString;
 
 @Data
 @NoArgsConstructor
@@ -35,26 +29,11 @@ public class PostResponse {
         this.id = post.getId();
         this.caption = post.getCaption();
         this.fileName = post.getFileName();
-        this.file = loadFileString(post);
+        this.file = loadFileString(post.getFilePath());
         this.contentType = post.getContentType();
         this.user = post.getUser();
         this.comments = post.getComments();
         this.likedBy = post.getLikedBy();
     }
 
-    private String loadFileString(Post post) throws IOException {
-        Resource resource = null;
-        if(!post.getFilePath().isEmpty()) {
-            resource = getResource(post.getFilePath());
-        }
-        byte[] fileBytes = Objects.requireNonNull(resource).getContentAsByteArray();
-        return Base64.encodeBase64String(fileBytes);
-    }
-
-    private Resource getResource(String filePath) throws FileNotFoundException, MalformedURLException {
-        if(!Files.exists(Path.of(filePath))) {
-            throw new FileNotFoundException(filePath + " was not found.");
-        }
-        return new UrlResource(Path.of(filePath).toUri());
-    }
 }
--- src/main/java/org/example/mysocialapp/util/FileUtils.java
+++ src/main/java/org/example/mysocialapp/util/FileUtils.java
@@ -1,10 +1,21 @@
 package org.example.mysocialapp.util;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.example.mysocialapp.entity.Post;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
-public class ImageUtils {
+public class FileUtils {
 
     public static byte[] compressImage(byte[] data) {
         Deflater deflater = new Deflater();
@@ -42,6 +53,22 @@ public class ImageUtils {
 
         }
         return outputStream.toByteArray();
+    }
+
+    public static String loadFileString(String filePath) throws IOException {
+        Resource resource = null;
+        if(!filePath.isEmpty()) {
+            resource = getResource(filePath);
+        }
+        byte[] fileBytes = Objects.requireNonNull(resource).getContentAsByteArray();
+        return Base64.encodeBase64String(fileBytes);
+    }
+
+    private static Resource getResource(String filePath) throws FileNotFoundException, MalformedURLException {
+        if(!Files.exists(Path.of(filePath))) {
+            throw new FileNotFoundException(filePath + " was not found.");
+        }
+        return new UrlResource(Path.of(filePath).toUri());
     }
 
 }
